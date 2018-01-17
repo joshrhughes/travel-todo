ec2195f2bdc4750e752a98c1d71a135a6447b44c
joshrhughes <josh@mtnpine.us>
2018-01-16 17:24:37 -0700

    heroku is the worst

--- server.js
+++ server.js
@@ -35,6 +35,4 @@ app.use(function (req, res, next) {
 var routes = require('./config/routes');
 app.use(routes);
 
-app.listen(process.env.PORT || 3000, function () {
-    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+app.listen(process.env.PORT || 3000);
