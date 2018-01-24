6e1edd0baa5a011d2ab7f78ca6ccd1ec0066536b
joshrhughes <josh@mtnpine.us>
2018-01-23 18:02:56 -0700

    updating style on login and sign up page

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ var session      = require('express-session');
 
 mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost/tripster'); 
 
+
 app.use(morgan('dev')); 
 app.use(cookieParser());
 app.use(bodyParser()); 
